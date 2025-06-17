ffb92d3e9b19896bec13566e658a78fc4d1c0bfe
Gulshan.DEV <132228055+gulshanchaudhari77@users.noreply.github.com>
2025-06-17 15:07:08 +0530

    Added edit

--- server.js
+++ server.js
@@ -7,5 +7,4 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/todos', todoRoutes);
 
-// app.listen(5000, () => console.log('Server running on port 5000'));
-app.listen(3306,()=> console.log('server ruuning  port 3306'));
+app.listen(5000, () => console.log('Server running on port 5000'));
